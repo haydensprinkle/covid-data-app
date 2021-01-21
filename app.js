7ed4fc5e8e680070d10a84a0ed157480956b0290
haydensprinkle <haydensprinkle@gmail.com>
2021-01-20 23:59:32 -0800

    quick update

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ window.addEventListener("load", () => {
   let caseDensityEl = document.querySelector(".case-density");
   let hospitalCapacityEl = document.querySelector(".hospital-capacity");
   let hospitalCovidUsageEl = document.querySelector(".hospital-covid-usage");
+  let hospitalTotalUsageEl = document.querySelector(".hospital-total-usage");
+  let icuCapacityEl = document.querySelector(".icu-capacity");
+  let icuCovidUsageEl = document.querySelector(".icu-covid-usage");
+  let icuTotalUsageEl = document.querySelector(".icu-total-usage");
   //variables for API request
   const apiKey = "1e93c10c57714916ac56744d86a5e208";
   const api = `https://api.covidactnow.org/v2/county/06097.json?apiKey=${apiKey}`;
@@ -29,7 +33,14 @@ window.addEventListener("load", () => {
       //variables to pull out data from API response
       let { county, state, country, population, lastUpdatedDate } = data;
       let { cases, deaths, newCases } = data.actuals;
-      let { capacity } = data.actuals.hospitalBeds;
+      let {
+        capacity,
+        currentUsageCovid,
+        currentUsageTotal,
+      } = data.actuals.hospitalBeds;
+      let icuCapacity = data.actuals.icuBeds.capacity;
+      let icuUsageCovid = data.actuals.icuBeds.currentUsageCovid;
+      let icuUsageTotal = data.actuals.icuBeds.currentUsageTotal;
       let { infectionRate, caseDensity } = data.metrics;
       let location = county + ", " + state + ", " + country;
       //inject extracted data into markup
@@ -42,5 +53,13 @@ window.addEventListener("load", () => {
       infectionRateEl.innerHTML = "Infection Rate: " + round(infectionRate, 2);
       caseDensityEl.innerHTML = "Case Density: " + round(caseDensity, 2);
       hospitalCapacityEl.innerHTML = "Total Hospital Beds: " + capacity;
+      hospitalCovidUsageEl.innerHTML =
+        "Hospital Beds in Use (Covid-19): " + currentUsageCovid;
+      hospitalTotalUsageEl.innerHTML =
+        "Hospital Beds in Use (Total): " + currentUsageTotal;
+      icuCapacityEl.innerHTML = "Total ICU Beds: " + icuCapacity;
+      icuCovidUsageEl.innerHTML =
+        "ICU Beds in Use (Covid-19): " + icuUsageCovid;
+      icuTotalUsageEl.innerHTML = "ICU Beds in Use (Total)" + icuUsageTotal;
     });
 });
