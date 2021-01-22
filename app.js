e88a41e8c2531ab13ed82675ba3f73728f45ab8d
haydensprinkle <haydensprinkle@gmail.com>
2021-01-21 22:58:59 -0800

    Added display for hospital and ICU beds remaining/created API call to get all county data

--- app.js
+++ app.js
@@ -12,16 +12,32 @@ window.addEventListener("load", () => {
   let hospitalCapacityEl = document.querySelector(".hospital-capacity");
   let hospitalCovidUsageEl = document.querySelector(".hospital-covid-usage");
   let hospitalTotalUsageEl = document.querySelector(".hospital-total-usage");
+  let hospitalRemainingEl = document.querySelector(".hospital-remaining");
   let icuCapacityEl = document.querySelector(".icu-capacity");
   let icuCovidUsageEl = document.querySelector(".icu-covid-usage");
   let icuTotalUsageEl = document.querySelector(".icu-total-usage");
+  let icuRemainingEl = document.querySelector(".icu-remaining");
   //variables for API request
   const apiKey = "1e93c10c57714916ac56744d86a5e208";
   const api = `https://api.covidactnow.org/v2/county/06097.json?apiKey=${apiKey}`;
+  const testApi = `https://api.covidactnow.org/v2/counties.json?apiKey=${apiKey}`;
   //function to round values to a desired precision
   let round = (value, precision) => {
     var multiplier = Math.pow(10, precision || 0);
     return Math.round(value * multiplier) / multiplier;
+  };
+  //function to format date
+  let formatDate = (date) => {
+    let dateString = date;
+    let formattedDate = new Date(dateString);
+    return (
+      formattedDate.getMonth() +
+      1 +
+      "/" +
+      formattedDate.getDate() +
+      "/" +
+      formattedDate.getFullYear()
+    );
   };
   //API request
   fetch(api)
@@ -44,22 +60,37 @@ window.addEventListener("load", () => {
       let { infectionRate, caseDensity } = data.metrics;
       let location = county + ", " + state + ", " + country;
       //inject extracted data into markup
-      dateUpdatedEl.innerHTML = "Date Update: " + lastUpdatedDate;
+      dateUpdatedEl.innerHTML = "Date Update: " + formatDate(lastUpdatedDate);
       locationEl.innerHTML = location;
-      populationEl.innerHTML = "Population: " + population;
-      totalCasesEl.innerHTML = "Total Cases: " + cases;
-      totalDeathsEl.innerHTML = "Total Deaths: " + deaths;
-      newCasesEl.innerHTML = "New Cases: " + newCases;
+      populationEl.innerHTML = "Population: " + population.toLocaleString();
+      totalCasesEl.innerHTML = "Total Cases: " + cases.toLocaleString();
+      totalDeathsEl.innerHTML = "Total Deaths: " + deaths.toLocaleString();
+      newCasesEl.innerHTML = "New Cases: " + newCases.toLocaleString();
       infectionRateEl.innerHTML = "Infection Rate: " + round(infectionRate, 2);
       caseDensityEl.innerHTML = "Case Density: " + round(caseDensity, 2);
-      hospitalCapacityEl.innerHTML = "Total Hospital Beds: " + capacity;
+      hospitalCapacityEl.innerHTML =
+        "Total Hospital Beds: " + capacity.toLocaleString();
       hospitalCovidUsageEl.innerHTML =
-        "Hospital Beds in Use (Covid-19): " + currentUsageCovid;
+        "Hospital Beds in Use (Covid-19): " +
+        currentUsageCovid.toLocaleString();
       hospitalTotalUsageEl.innerHTML =
-        "Hospital Beds in Use (Total): " + currentUsageTotal;
-      icuCapacityEl.innerHTML = "Total ICU Beds: " + icuCapacity;
+        "Hospital Beds in Use (Total): " + currentUsageTotal.toLocaleString();
+      hospitalRemainingEl.innerHTML =
+        "Hospital Beds Remaining: " + (capacity - currentUsageTotal);
+      icuCapacityEl.innerHTML =
+        "Total ICU Beds: " + icuCapacity.toLocaleString();
       icuCovidUsageEl.innerHTML =
-        "ICU Beds in Use (Covid-19): " + icuUsageCovid;
-      icuTotalUsageEl.innerHTML = "ICU Beds in Use (Total)" + icuUsageTotal;
+        "ICU Beds in Use (Covid-19): " + icuUsageCovid.toLocaleString();
+      icuTotalUsageEl.innerHTML =
+        "ICU Beds in Use (Total): " + icuUsageTotal.toLocaleString();
+      icuRemainingEl.innerHTML =
+        "ICU Beds Remaining: " + (icuCapacity - icuUsageTotal);
+    });
+  fetch(testApi)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
     });
 });
